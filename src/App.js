6ed78e22e8ed59d2bd8541eae6f8e4ae9ad95a5b
Ankit Negi <34399945+ankitn1311@users.noreply.github.com>
2021-12-15 13:32:28 +0530

    from github

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">New app</div>;
+  return <div className="App">New app changed from github</div>;
 }
 
 export default App;
